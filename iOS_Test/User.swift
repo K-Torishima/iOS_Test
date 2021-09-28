0d08ae5ae0681368d9347db5dc2f255b3fc8da2b
koji-torishima <torikou3685@gmail.com>
2021-09-28 23:51:20 +0900

    忘れているので復習

---
+++ iOS_Test/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  iOS_Test
+//
+//  Created by 鳥嶋 晃次 on 2021/09/28.
+//
+
+import Foundation
+
+class User {
+    var name: String = "koji"
+    var age: Int = 30
+    var isMale: Bool = true
+    var isFemale: Bool = false
+    var catName: String? = nil
+}
+
+
--- iOS_TestTests/iOS_TestTests.swift
+++ iOS_TestTests/iOS_TestTests.swift
@@ -6,27 +6,50 @@
 //
 
 import XCTest
+
+// ↓ これがないとテストしたいモジュールにアクセスできない
 @testable import iOS_Test
 
 class iOS_TestTests: XCTestCase {
-
+    
+    var user = User()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        print("setUp")
+        // ここでインスタンスを書き換えたりできる、通る通らないがわかると良さそう
+        user.age = 30
+        // テストの開始時に最初に呼ばれる関数、テストケースを回すために必要な設定やインスタンスの生成などをここで行う
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        print("tearDown")
+        // テスト終了時に一度だけ呼ばれる関数
+
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // テスト対象の関数
+        // この関数一つに対してテストケースを一つ書く。
+        // 第一引数と第二引数が等しいことを検査するためのテスト
+        XCTAssertEqual(user.age, 30) // 等しいのでテスト通過
+        // XCTAssertEqual(calculator.age, 20) // 等しくないのでテスト通過しない
+        //　第一引数と第二引数が等しくないときに検査するためのテスト
+        XCTAssertNotEqual(user.age, 20) // 等しくないためテスト通過
+        // XCTAssertNotEqual(calculator.age, 30) // 等しいためテスト通過しない
+        
+        // test対象がtrueかどうか
+        XCTAssertTrue(user.isMale)
+        // 検査対象がFalseかどうか
+        XCTAssertFalse(user.isFemale)
+        // 検査対象がnilかどうか
+        XCTAssertNil(user.catName)
+
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
+        // パフォーマンス測定の関数
         self.measure {
-            // Put the code you want to measure the time of here.
+            // 計算したい処理を記載
         }
     }
 
